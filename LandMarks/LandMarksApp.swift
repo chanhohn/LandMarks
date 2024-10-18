f9dbdac389f4a92f8d6319add82d4f450e9767f1
chanhohn <ciwei17@gmail.com>
2024-10-18 17:03:53 +0800

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by hohn on 2024/10/18.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
