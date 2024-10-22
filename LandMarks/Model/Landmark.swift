16e1f30aa4a187edfed2ae437cf797d8ad23de49
chanhohn <ciwei17@gmail.com>
2024-10-22 23:30:51 +0800

    update model

---
+++ LandMarks/Model/Landmark.swift
@@ -0,0 +1,41 @@
+//
+//  Landmark.swift
+//  LandMarks
+//
+//  Created by hohn on 2024/10/20.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+//  一些基本的变量信息
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    
+//定义一个Image类型的变量
+    var image:Image{
+        Image(imageName)
+    }
+    
+//
+    private var coordinates: Coordinates
+//
+    var locationCoordinate:CLLocationCoordinate2D{
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+//    定义了一个结构体
+    struct Coordinates: Hashable,Codable{
+        var latitude:Double
+        var longitude:Double
+    }
+}
---
+++ LandMarks/Views/LandmarkRow.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkRow.swift
+//  LandMarks
+//
+//  Created by hohn on 2024/10/22.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        
+        HStack{
+            landmark.image
+                .resizable()
+                .frame(width: 50,height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+        
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[1])
+}
